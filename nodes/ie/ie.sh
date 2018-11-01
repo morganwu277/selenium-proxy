797d15c466d7f4af06ad5e38b23159ccf0b28ba7
morganwu277 <xue777hua@gmail.com>
2018-11-01 16:00:39 -0400

    use different net range for ie

--- nodes/ie/ie.sh
+++ nodes/ie/ie.sh
@@ -6,7 +6,7 @@ cd $DIR
 
 . ./../common.sh
 
-private_ip="192.168.34.24"
+private_ip="192.168.35.20"
 browserName="ie"
 
 cp ./../selenium/selenium-server-standalone-3.14.0.jar selenium-$browserName/
