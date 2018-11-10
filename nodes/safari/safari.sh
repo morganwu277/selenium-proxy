a4b2f3ad58f6a4922a287b5889f9d8ad1c649252
morganwu277 <xue777hua@gmail.com>
2018-11-09 22:38:51 -0500

    add Safari process check

--- nodes/safari/safari.sh
+++ nodes/safari/safari.sh
@@ -27,7 +27,8 @@ fi
 
 if [[ "$1" == "stop" ]]; then
     echo "stop... $browserName"
-    ps -ef|grep safaridriver |grep -v grep |awk '{print $2}'|xargs kill
+    ps -ef|grep 'safaridriver' |grep -v grep |awk '{print $2}'|xargs kill
+    ps -ef|grep 'Safari --automation' |grep -v grep |awk '{print $2}'|xargs kill
     bg_stop_selenium_node
 fi
 
@@ -46,7 +47,9 @@ if [[ "$1" == "status" ]]; then
     # TODO: checking if node.json contains this safari?
     if ps -ef | grep node.json | grep -v -q grep; then
       echo "up"
-    elif ps -ef | grep safaridriver | grep -v -q grep; then
+    elif ps -ef | grep 'safaridriver' | grep -v -q grep; then
+      echo "up"
+    elif ps -ef | grep 'Safari --automation' | grep -v -q grep; then
       echo "up"
     else
       echo "down"
